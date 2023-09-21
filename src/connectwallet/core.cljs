3a1d9fb40f454e627951f386fb182fc7bbd00e34
Alexandre El-Khoury <alexkhoury@hotmail.com>
2023-09-21 15:31:48 +0200

    errors solved

--- src/connectwallet/core.cljs
+++ src/connectwallet/core.cljs
@@ -27,44 +27,19 @@
 
 (def ethereumClient (EthereumClient. wagmiConfig chains))
 
-(defn prints []
-  (.log js/console chains)
-  (.log js/console publicClient)
-  (.log js/console ethereumClient)
-  (.log js/console Web3Button)
-  (.log js/console Web3Modal)
-  (.log js/console wagmiConfig))
-
 (defn Application []
   (js/console.log "Hello world!")
   [:<>
    [:div
     [:h1 "Wallet Connect V2"]
-    [:button {:on-click prints} "Print"]
     [:div
      [:h2 "my web3 component"]
-     [:WagmiConfig {:config wagmiConfig}
-      [:Web3Button]]
-     [:Web3Modal {:projectId projectId
-                  :ethereumClient ethereumClient}]]]])
+     [:> WagmiConfig {:config wagmiConfig}
+      [:> Web3Button]]
+     [:> Web3Modal {:projectId projectId
+                    :ethereumClient ethereumClient}]]]])
 
 (defn app []
   (let [container (js/document.getElementById  "app")
         root (rdom/createRoot container)]
     (.render root (r/as-element [Application]))))
-
-;; (dom/render [Application] (js/document.getElementById "app"))
-
-;; (defn openWeb3Modal []
-;;   (.open (useWeb3Modal)))
-
-;; (defn web-button []
-;;   [:Web3Button])
-
-;; (defn wagmi-config []
-;;   [:WagmiConfig {:config wagmiConfig}
-;;    [web3-button]])
-
-;; (defn web3-modal []
-;;   [:Web3Modal {:projectId projectId
-;;                :ethereumClient ethereumClient}])
